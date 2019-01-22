1ff11fd4a7f9213efab045ad601afadfcfd753d2
pavelmakarets <makaretsp@mail.ru>
2019-01-22 13:49:22 +0200

    first success

--- force-app/main/default/lwc/testComponent/testComponent.js
+++ force-app/main/default/lwc/testComponent/testComponent.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class TestComponent extends LightningElement {}
+export default class TestComponent extends LightningElement {
+    @api name;
+}
